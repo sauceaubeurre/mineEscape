ca81b044196014f4d08c25027a7dfde94b306bd4
Ecrivateur <lng2@free.fr>
2016-05-29 18:31:53 +0200

    update interlaced fic resolved

---
+++ src/main/Ennemy.java
@@ -0,0 +1,170 @@
+package main;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
+public class Ennemy {
+	
+	private float dx = 0, dy = 0;
+	private float x , y;
+	private int direction = 2;
+	private static final float speed = .05f;
+	private boolean onStair;
+	
+	private Animation[] animations = new Animation[12];
+	private Map map;
+
+	public Ennemy(Map map, float x, float y) {
+		this.map = map;
+		this.x = x;
+		this.y = y;
+	}
+	public void init() throws SlickException {
+		SpriteSheet spriteSheet = new SpriteSheet("sprites/character2.png", 32, 32);
+		this.animations[0] = loadAnimation(spriteSheet, 0, 1, 7);
+	    this.animations[1] = loadAnimation(spriteSheet, 0, 1, 5);
+	    this.animations[2] = loadAnimation(spriteSheet, 0, 1, 4);
+	    this.animations[3] = loadAnimation(spriteSheet, 0, 1, 6);
+	    this.animations[4] = loadAnimation(spriteSheet, 1, 3, 7);
+	    this.animations[5] = loadAnimation(spriteSheet, 1, 3, 5);
+	    this.animations[6] = loadAnimation(spriteSheet, 1, 3, 4);
+	    this.animations[7] = loadAnimation(spriteSheet, 1, 3, 6);
+	    this.animations[8] = loadAnimation(spriteSheet, 3, 6, 7);
+	    this.animations[9] = loadAnimation(spriteSheet, 3, 6, 5);
+	    this.animations[10] = loadAnimation(spriteSheet, 3, 6, 4);
+	    this.animations[11] = loadAnimation(spriteSheet, 3, 6, 6);
+	  }
+
+	private Animation loadAnimation(SpriteSheet spriteSheet, int startX, int endX, int y) {
+		Animation animation = new Animation();
+		for (int x = startX; x < endX; x++) {
+			animation.addFrame(spriteSheet.getSprite(x, y), 100);
+		}
+		return animation;
+	}
+	
+	public boolean isMoving() {
+		return dx != 0 || dy != 0;
+	}
+
+	public void render(Graphics g) {
+		g.drawAnimation(animations[direction + (isMoving() ? 4 : 0)], (int) x - 16, (int) y - 30);
+	}
+	
+	
+	
+	public void update(int delta) {
+		if (this.isMoving()) {
+			updateDirection();
+			float futurX = getFuturX(delta);
+			float futurY = getFuturY(delta);
+			boolean collision = this.map.isCollision(futurX, futurY);
+			if (collision) {
+				stopMoving();
+			} else {
+				this.x = futurX;
+				this.y = futurY;
+			}
+		}
+	}
+	
+	private float getFuturX(int delta) {
+		return this.x + speed * delta * dx;
+	}
+
+	private float getFuturY(int delta) {
+		float futurY = this.y + speed * delta * dy;
+		if (this.onStair) {
+			futurY = futurY - speed * dx * delta;
+		}
+		return futurY;
+	}
+	
+	private void updateDirection() {
+		if (dx > 0 && dx >= Math.abs(dy)) {
+			direction = 3;
+		} else if (dx < 0 && -dx >= Math.abs(dy)) {
+			direction = 1;
+		} else if (dy < 0) {
+			direction = 0;
+		} else if (dy > 0) {
+			direction = 2;
+		}
+	}
+	
+	public float getDx() {
+		return dx;
+	}
+	public void setDx(float dx) {
+		this.dx = dx;
+	}
+	public float getDy() {
+		return dy;
+	}
+	public void setDy(float dy) {
+		this.dy = dy;
+	}
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
+	public int getDirection() {
+		return direction;
+	}
+	public void setDirection(int px, int py) {
+		//set ennemy Dx
+				if(x > px + 5){
+					dx = -1;
+				}
+				else if(x < px - 5){
+					dx = 1;
+				}
+				else{
+					dx = 0;
+				}
+				
+				//set ennemy Dy
+				if(y > py + 5){
+					dy = -1;
+				}
+				else if(y < py - 5){
+					dy = 1;
+				}
+				else{
+					dy = 0;
+				}
+	
+	}
+	
+	public void stopMoving() {
+		dx = 0;
+		dy = 0;
+	}
+	
+	public Animation[] getAnimations() {
+		return animations;
+	}
+	public void setAnimations(Animation[] animations) {
+		this.animations = animations;
+	}
+	public Map getMap() {
+		return map;
+	}
+	public void setMap(Map map) {
+		this.map = map;
+	}
+	public static float getSpeed() {
+		return speed;
+	}
+	
+}
---
+++ src/main/Item.java
@@ -0,0 +1,71 @@
+package main;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
+public class Item {
+	private int time = 0;
+	private float hp = 4;
+	private float x, y;
+	
+	private Image[] image = new Image[5];
+
+	public Item(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void init() throws SlickException {
+		SpriteSheet spriteSheet = new SpriteSheet("sprites/rock.png", 32, 32);
+		image[4] = spriteSheet.getSprite(0, 0);
+		image[3] = spriteSheet.getSprite(1, 0);
+		image[2] = spriteSheet.getSprite(2, 0);
+		image[1] = spriteSheet.getSprite(3, 0);
+		image[0] = spriteSheet.getSprite(4, 0);
+	}
+
+	public void render(Graphics g, Player player) {
+		g.drawImage(image[(int)(hp)], x, y);
+	}
+	
+	public void update(Player player, int delta){
+		if(player.isInHitbox(x, y) == true){
+			//System.out.println(time);
+			if((time - 1000) > 0 && hp != 0 && player.isMining() == true){
+				//System.out.println("hit rock");
+				hp--;
+				time = 0;
+			}else if(player.isMoving() == true && hp != 0){
+				if(player.isLeft(x, y) == true && player.getDx() == 1){
+					player.setDx(0);
+				}if(player.isRight(x, y) && player.getDx() == -1){
+					player.setDx(0);
+				}if(player.isTop(x, y) && player.getDy() == 1){
+					player.setDy(0);
+				}if(player.isBot(x, y) && player.getDy() == -1){
+					player.setDy(0);
+				}
+				time = 0;
+			}
+		}
+		time += delta;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+}
